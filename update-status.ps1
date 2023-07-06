d3a6e1ba9484862c7aec9ad19f548313555e527e
David Koprivec <koprivecd@gmail.com>
2023-07-06 07:32:34 +0200

    move functions

--- update-status.ps1
+++ update-status.ps1
@@ -1,65 +1,6 @@
 ﻿Add-Type -AssemblyName System.Windows.Forms
 
-
-function Read-MessageBoxDialog([string]$Message, [string]$WindowTitle, [System.Windows.Forms.MessageBoxButtons]$Buttons = [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]$Icon = [System.Windows.Forms.MessageBoxIcon]::None)
-{
-    return [System.Windows.Forms.MessageBox]::Show($Message, $WindowTitle, $Buttons, $Icon)
-}
-
-
-function UpdateStatus {
-    Write-Host "Updating Slack status"
-
-
-    $dateTime = Get-Date
-    $localTimeZone = Get-TimeZone
-
-    $endOfDay=[DateTime]::Today.AddDays(1).AddSeconds(-1).AddSeconds($localTimeZone.GetUtcOffset($dateTime).TotalSeconds * -1)
-    $endOfDayEpoch = Get-Date $endOfDay -UFormat %s
-
-
-    $payload = @{
-        "profile" = @{
-            "status_text" = $newStatusText
-            "status_emoji" = $newStatusEmoji
-            "status_expiration" = $endOfDayEpoch
-        }
-    } | ConvertTo-Json
-
-    $response = Invoke-RestMethod -Uri $slackStatusUrl -Method POST -Headers @{
-        "Authorization" = "Bearer $accessToken"
-        "Content-Type" = "application/json"
-    } -Body $payload
-   
-    if ($response.ok) {
-        Write-Host "Slack status updated successfully."
-    } else {
-        Write-Host "Failed to update Slack status. Error: $($response.error)"
-    }
-}
-
-function GetStatus {
-    Write-Host "Getting slack status for user: $userID"
-
-    $apiUrl = "https://slack.com/api/users.profile.get?user=$userID"
-    $response = Invoke-RestMethod -Uri $apiUrl -Headers @{
-        "Authorization" = "Bearer $accessToken"
-        "Content-Type" = "application/x-www-form-urlencoded"
-    }
-
-    if ($response.ok) {
-        $statusEmoji = $response.profile.status_emoji
-        $statusMessage = $response.profile.status_text
-        #Write-Host "Status emoji: $statusEmoji and status message: $statusMessage"
-
-        return ![string]::IsNullOrEmpty($statusMessage) -or ![string]::IsNullOrEmpty($statusEmoji)
-    }
-    else {
-        Write-Host "Error: $($response.error)"
-        return true
-    }
-
-}
+. .\update-status-utils.ps1
 
 $envFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".env"
 $envVariables = Get-Content $envFilePath
@@ -83,7 +24,7 @@ $homeWifiName = "Error 404"
 $officeWifiName1 = "Orange Black"
 $officeWifiName2 = "House of Cards"
 
-$wifiName = (Get-NetConnectionProfile).Name
+$wifiName = Get-WiFiName
 
 Write-Host "Connected Wi-Fi Network: $wifiName"
 
@@ -110,10 +51,10 @@ if ($wifiName -eq $homeWifiName) {
 
 
 $currentHour = (Get-Date).Hour
-$isCurrentlyStatusSet = GetStatus
+$isCurrentlyStatusSet = Get-Status
 
 if ($currentHour -ge $commingToWorkStart -and $currentHour -lt $commingToWorkEnd -and -not $isCurrentlyStatusSet) {
-    UpdateStatus
+    Update-Status
 } else {
 
     if ($isCurrentlyStatusSet) {
@@ -123,11 +64,11 @@ if ($currentHour -ge $commingToWorkStart -and $currentHour -lt $commingToWorkEnd
     }
 
     if ($buttonClicked -eq "Yes") { 
-        UpdateStatus
+        Update-Status
 		Write-Host ('-'*20)
     }
     else { 
-        Write-Host 'Ok, bye.'
+        Write-Host 'Action canceled, bye.'
 		Write-Host ('-'*20)
     }
 }
